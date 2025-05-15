3f7ec6a47b00287098687568aadeafffdf9a57f3
motifoye <yasquar@ya.ru>
2025-05-16 02:43:42 +0700

    pointcod

--- WpfApp1/Point2D.cs
+++ WpfApp1/Point2D.cs
@@ -8,5 +8,33 @@ namespace WpfApp1
 {
     internal class Point2D
     {
+        //Атрибуты класса
+        private int X;
+        private int Y;
+        //Конструктор класса
+        public Point2D(int x, int y)
+        {
+        //this используется для однозначного указания на атрибуты класса так как переменные имеют одинаковые имена
+            this.X = x;
+            this.Y = y;
+        }
+        //Методы для получения координат
+        public int getX()
+        {
+            return X;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+        //Методы для изменения координат
+        public void addX(int x)
+        {
+            X += x;
+        }
+        public void addY(int y)
+        {
+            Y += y;
+        }
     }
 }
