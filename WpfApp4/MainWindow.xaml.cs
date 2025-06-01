a6329b6e435f4c74c1aab4d64e9bb666725f8551
motifoye <yasquar@ya.ru>
2025-06-02 01:51:51 +0700

    --

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.Controls;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new MainControl();
+        }
+
+        private void Quizzes_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new QuizListControl();
+        }
+
+        private void Questions_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new QuestionListControl();
+        }
+    }
+}
